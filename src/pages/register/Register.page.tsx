f6bdc841f3698aef4687051ba228e395bd6c7ec7
Narek Sargsyan <52748705+NSarg1@users.noreply.github.com>
2025-01-23 01:08:25 +0400

    Update Register.page.tsx

--- src/pages/register/Register.page.tsx
+++ src/pages/register/Register.page.tsx
@@ -11,6 +11,7 @@ import { VehicleProps } from '../chargers/components/Charger/components/VehicleF
 const RegisterPage = () => {
   const { setVehicles, vehicles } = useContext(AppContext);
   const navigate = useNavigate();
+
   const handleRegister = (values: VehicleProps) => {
     setVehicles((prev: VehicleProps[]) => [
       ...prev,
@@ -26,16 +27,28 @@ const RegisterPage = () => {
           <h1>New Car Registration</h1>
           <Form className={styles.list} onFinish={handleRegister}>
             <div className={styles.main}>
-              <Form.Item required name="name">
+              <Form.Item
+                name="name"
+                rules={[{ required: true, message: 'Please enter the name!' }]}
+              >
                 <Input placeholder="Name" />
               </Form.Item>
-              <Form.Item required name="plate">
+              <Form.Item
+                name="plate"
+                rules={[{ required: true, message: 'Please enter the plate number!' }]}
+              >
                 <Input placeholder="Plate number" />
               </Form.Item>
-              <Form.Item required name="telephone">
+              <Form.Item
+                name="telephone"
+                rules={[{ required: true, message: 'Please enter a Telegram ID or telephone number!' }]}
+              >
                 <Input placeholder="Telegram ID / Telephone" />
               </Form.Item>
-              <Form.Item required name="model">
+              <Form.Item
+                name="model"
+                rules={[{ required: true, message: 'Please enter the vehicle type!' }]}
+              >
                 <Input placeholder="Vehicle type" />
               </Form.Item>
             </div>
